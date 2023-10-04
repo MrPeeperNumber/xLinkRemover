a5e48ef55eacc673b6ecf90fe161d61eb5f09230
MrPeeperNumber <jdroberts7726@gmail.com>
2023-10-04 14:17:38 -0700

    Fixed functionality for furaffinity links, and added some basic documentation

--- convert.js
+++ convert.js
@@ -1,3 +1,4 @@
+// Initialize array of links that will be used
 const links = [
 	"https://twitter.com",
 	"https://vxtwitter.com",
@@ -5,33 +6,48 @@ const links = [
 	"https://vxfuraffinity.net"
 ];
 
+// Function definition for converting messages to 
 const convertMessage = (client, msg) => {
-	console.log(`Convert command from ${msg.from.username}: ${msg.from.id}`);
+	// Log the function used, username, user id, and the contents
+	console.log(`Convert command from ${msg.from.username}: ${msg.from.id}\n\tMessage content: ${msg.text}`);
 
+	// Convert the provided x.com link into 3 different links
+	// twitter, vxtwitter, and fxtwitter links
+	// Concatenates the post information onto the end of the links in the link array
+	// Sends a message to the user with the 3 new links
 	client.sendMessage(msg.chat.id, `Here are your new and improved links\\! Click the link text to have it automatically copied to your clipboard\\.\n\n\`twitter\`:\n\`${links[0].concat(msg.text.slice(msg.text.indexOf("x.com")+5))}\`\n\n\`vxtwitter\`:\`${links[1].concat(msg.text.slice(msg.text.indexOf("x.com")+5))}\`\n\n\`fxtwitter\`:\`${links[2].concat(msg.text.slice(msg.text.indexOf("x.com")+5))}\``, 
 		{ parse_mode: "MarkdownV2" }
 	);
 };
 
+// Function definition for handling inline queries
 const handleInlineQuery = (client, query) => {
-	let results = [];
+	// Conditional for handling x.com or furaffinity.net links
+	// Bot will only listen for a query when it receives either of those two links
+	if(query.query.includes("x.com") || query.query.includes("furaffinity.com")) {
+		if(query.query.includes("x.com")) {
+			// Log inline query request, who it is from, and their user ID
+			console.log(`Inline query from ${query.from.username}: ${query.from.id}\n\tQuery Contents: ${query.query}`);
 
-	switch (query.query) {
-		case query.query.includes("x.com"):
-			console.log(`Inline query from ${query.from.username}: ${query.from.id}`);
-			results = [{
+			// Set results array to be an article object that holds the converted link for vxtwitter only
+			const results = [{
 				id: "1",
 				type: "article",
 				title: `${links[1].concat(query.query.slice(query.query.indexOf("x.com")+5))}`,
 				input_message_content: {
 					message_text: `${links[1].concat(query.query.slice(query.query.indexOf("x.com")+5))}`
 				}
-			}];		
+			}];
+
+			// Send the query results back to the user
 			client.answerInlineQuery(query.id, results);
-			break;
-		case query.query.includes("furaffinity.net"):
-			console.log(`Inline query from ${query.from.username}: ${query.from.id}`);
-			results = [{
+		}
+		else if(query.query.includes("furaffinity.net")) {
+			// Log inline query request, who it is from, and their user ID
+			console.log(`Inline query from ${query.from.username}: ${query.from.id}\n\t${query.query}`);
+
+			//Set results array to be an article object that holds the converted link for vxfuraffinity
+			const results = [{
 				id: "1",
 				type: "article",
 				title: `${links[3].concat(query.query.slice(query.query.indexOf("furaffinity.net")+5))}`,
@@ -39,22 +55,10 @@ const handleInlineQuery = (client, query) => {
 					message_text: `${links[3].concat(query.query.slice(query.query.indexOf("furaffinity.net")+5))}`
 				}
 			}];
-			client.answerInlineQuery(query.id, results);
-			break;
-	}
 
-	if(query.query.includes("x.com") || query.query.includes("furaffinity.com")) {
-		console.log(`Inline query from ${query.from.username}: ${query.from.id}`);
-		const results = [{
-			id: "1",
-			type: "article",
-			title: `${links[1].concat(query.query.slice(query.query.indexOf("x.com")+5))}`,
-			input_message_content: {
-				message_text: `${links[1].concat(query.query.slice(query.query.indexOf("x.com")+5))}`
-			}
-		}];
-	
-		client.answerInlineQuery(query.id, results);
+			//Send the query results back to the user
+			client.answerInlineQuery(query.id, results);
+		}
 	}
 };
 
--- index.js
+++ index.js
@@ -1,23 +1,37 @@
+// Initialize library for using environment variables
 require("dotenv").config();
 
+// Initialize bot API library
 const CLIENT = require("node-telegram-bot-api");
+
+// Check for a token, otherwise, throw an error
 if(process.env.TOKEN == null || process.env.TOKEN === ""){
 	throw new Error("Could not find Telegram token");
 }
+
+// Connect the bot client to Telegram servers
 const client = new CLIENT(process.env.TOKEN, { polling: true });
+
+// Initialize the module that holds the conversion functions
 const convert = require("./convert.js");
 
+// List of commadns that the bot can take
 const commands = [
 	"/start",
 	"/help",
 	"/info"
 ];
 
+// When the bot client receives a message
 client.onText(/.*/, (msg) => {
+	// Check if the text includes an x.com link
 	if (msg.text.includes("x.com")) {
+		// Call the message conversion funciton if it does
 		convert.convertMessage(client, msg);
 	}
+	//If not, check if the message is a command
 	else {
+		// Check the list of commands against the message text
 		switch(msg.text) {
 			case commands[0]:
 				startCommand(msg);
@@ -30,8 +44,10 @@ client.onText(/.*/, (msg) => {
 				break;
 		}
 	}
+	// If neither, do nothing
 });
 
+// Function definition for the start command
 const startCommand = (msg) => {
 	console.log(`Start command from ${msg.from.username}: ${msg.from.id}`);
 	client.sendMessage(msg.chat.id, "Welcome to the X Link Remover Bot\\!\nI remove the pesky \"x\" from `x.com` links\\! Just send the link, and it will be converted into `twitter`, `vxtwitter`, and `fxtwitter` links\\.\n For more information about `vxtwitter` and `fxtwitter` use the /info command\\.\nFor other help, use the /help command\\.", 
@@ -39,6 +55,7 @@ const startCommand = (msg) => {
 	);
 };
 
+// Function definition for info command
 const infoCommand = (msg) => {
 	console.log(`Info command from ${msg.from.username}: ${msg.from.id}`);
 	client.sendMessage(msg.chat.id, "`vxtwitter` and `fxtwitter` are services that uses the X \\(Twitter\\) API to fix image and video embedding\\. Currently, X \\(Twitter\\) is hit or miss about when it will embed images or videos in Telegram\\. Sometimes it works, sometimes it doesn't\\, and sometimes it only kinda works\\? `vxtwitter` fixes that\\.\nMore information about them can be found on their GitHub Repositories:\n\n`vxtwitter`:\n`https://github\\.com/dylanpdx/BetterTwitFix`\n\n`fxtwitter`:\n`https://github.com/FixTweet/FixTweet`", 
@@ -49,6 +66,7 @@ const infoCommand = (msg) => {
 	);
 };
 
+// Function definition for help command
 const helpCommand = (msg) => {
 	console.log(`Help command from ${msg.from.username}: ${msg.from.id}`);
 	client.sendMessage(msg.chat.id, "To get your new links, just copy and paste your `x\\.com` link into the chat, and it will automatically be converted into `twitter\\.com`, `vxtwitter`, and `fxtwitter` links\\. Just click on the link you would like, and it will be automatically copied to your clipboard\\!\n\n**__Inline Queries:__**\nIn any chat, just type `@noMoreXbot`, then paste your `x.com`, and you will automatically get a `vxtwitter` link that you can send to the chat\\!", 
@@ -56,6 +74,7 @@ const helpCommand = (msg) => {
 	);
 };
 
+// Function listening for inline queries
 client.on("inline_query", (query) => {
 	convert.handleInlineQuery(client, query);
 });
